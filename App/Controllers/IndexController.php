dbdcaa96b8ba6adc3326f53d9f506e4f83d89019
joao <menezes.jrafael@gmail.com>
2020-10-04 23:04:07 -0300

    first commit

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController
+{
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new \stdClass();
+    }
+
+    public function index()
+    {
+        $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+        $this->render('index');
+    }
+
+    public function sobreNos()
+    {
+        $this->view->dados = array('Notebook', 'Smartphone');
+        $this->render('sobreNos');
+    }
+
+    public function render($view)
+    {
+        $classAtual = get_class($this);
+        $classAtual = str_replace('App\\Controllers\\', '', $classAtual);
+        $classAtual = strtolower(str_replace('Controller', '', $classAtual));
+
+        require_once "../App/Views/".$classAtual."/". $view.".phml";
+    }
+}
+
+?>
